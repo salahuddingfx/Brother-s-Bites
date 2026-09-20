8fc67a0e44e0a05dbe17886a6dd9c8a6b5facfef
salahuddingfx <salauddinkaderappy@gmail.com>
2026-09-20 08:53:36 +0600

    chore: update CORS origins and environment variable URLs with new domain

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,7 +18,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://127.0.0.1:3000',
-  'https://brothers-bites.salahuddin.codes',
+  'https://bbites.salahuddin.codes',
 ].filter(Boolean);
 
 app.use(
